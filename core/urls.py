bca090ed825bc86b7da4d9d6fc966d9f87138c8e
fady <gamilfady605@gmail.com>
2022-03-05 06:12:11 +0200

    2 databases are setup, the setup of jwt auth is done

---
+++ core/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+
+# require the built-in class-based views from simpleJWT to utilize in our auth api
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
+    path('api/token/verify/', TokenVerifyView.as_view()),
+]
